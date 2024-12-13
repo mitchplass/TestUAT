0ab336c95097178a91c6fe010fd0029bd7bde1bd
CORP\Mitchell.Plass <mitchell.plass@wisetechglobal.com>
2024-12-13 14:51:44 +1100

    Add project files.

---
+++ Hooks/Hooks.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium.Appium;
+using TestUAT.Drivers;
+
+namespace TestUAT.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+        public static AppiumDriver Driver;
+
+        [BeforeScenario]
+        public void Setup()
+        {
+            string platform = "android";
+            Driver = AppiumDriverFactory.CreateDriver(platform);
+        }
+
+        [AfterScenario]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+    }
+}
