32fd9e02f435eeb02456eee731dc3d9f6d78578c
CORP\Mitchell.Plass <mitchell.plass@wisetechglobal.com>
2025-01-30 16:23:29 +1100

    open port transport working test

--- Features/OpenApp.feature.cs
+++ Features/OpenApp.feature.cs
@@ -99,9 +99,13 @@ this.ScenarioInitialize(scenarioInfo);
  testRunner.And("I press port transport", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
 #line 9
- testRunner.And("I enter login details with username \"<username>\" and password \"<password>\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+ testRunner.And("I enter login details with username \"mitchell.plass@wisetechglobal.com\" and passw" +
+                        "ord \"M1tchW1se.2911!\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
 #line 10
+ testRunner.And("I select account \"Demo.User.1\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 11
  testRunner.Then("the port transport portal should be open", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
             }
